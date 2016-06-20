ba4d391a40468676051a6fdd7bcb2f66f8a0c034
Peter Maselkowski <pmaselkowski@gmail.com>
2016-06-20 11:42:29 +0200

    ! Fixed missing use statements

--- src/config/mangan.cfg.php
+++ src/config/mangan.cfg.php
@@ -1,7 +1,19 @@
 <?php
 
+use Maslosoft\Mangan\Sanitizers\DateSanitizer;
+use Maslosoft\Mangan\Sanitizers\DateWriteUnixSanitizer;
+use Maslosoft\Mangan\Sanitizers\MongoObjectId;
+use Maslosoft\Mangan\Sanitizers\MongoWriteStringId;
+use Maslosoft\Manganel\Filters\SearchFilter;
+use Maslosoft\Manganel\SearchArray;
+
 // Mangan additional configuration
 return [
+	'filters' => [
+		SearchArray::class => [
+			SearchFilter::class,
+		],
+	],
 	'sanitizersMap' => [
 		SearchArray::class => [
 			MongoObjectId::class => MongoWriteStringId::class,
