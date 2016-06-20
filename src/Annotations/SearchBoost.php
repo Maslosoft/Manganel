2f3132d44a14b8e5458898bde1adbea275d81d8c
Peter Maselkowski <pmaselkowski@gmail.com>
2016-06-20 12:48:43 +0200

    +~ Search boost draft

---
+++ src/Annotations/SearchBoost.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Maslosoft\Manganel\Annotations;
+
+use Maslosoft\Manganel\Meta\ManganelTypeAnnotation;
+
+/**
+ * SearchBoost
+ * 
+ * Use this annotation to increase or decreate importance of field in search
+ * results. Use positive values to increase importance, and negative to
+ * decrease.
+ * 
+ * Increase importance example:
+ * 
+ * ```php
+ * @SearchBoost(3)
+ * ```
+ * 
+ * Decrease importance example:
+ * 
+  ```php
+ * @SearchBoost(-3)
+ * ```
+ * 
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class SearchBoost extends ManganelTypeAnnotation
+{
+
+	const Ns = __NAMESPACE__;
+
+	public function init()
+	{
+		throw new \Exception('Not implemented');
+	}
+
+}
