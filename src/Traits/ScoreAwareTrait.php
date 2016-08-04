c6453d86aa9ef96effccb629596340e0859f314f
Peter Maselkowski <pmaselkowski@gmail.com>
2016-08-04 14:43:42 +0200

    + Support for setting index on found models + Support for setting score on found models

---
+++ src/Traits/ScoreAwareTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Maslosoft\Manganel\Traits;
+
+use Maslosoft\Manganel\Interfaces\ScoreAwareInterface;
+use UnexpectedValueException;
+
+/**
+ * ScoreAwareTrait
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class ScoreAwareTrait
+{
+
+	private $score = 0.0;
+
+	public function getScore()
+	{
+		if (!$this instanceof ScoreAwareInterface)
+		{
+			throw new UnexpectedValueException(sprintf('Class `%s` using `%s` must implement `%s`', get_class($this), __CLASS__, ScoreAwareInterface::class));
+		}
+		return $this->score;
+	}
+
+	public function setScore($score)
+	{
+		if (!$this instanceof ScoreAwareInterface)
+		{
+			throw new UnexpectedValueException(sprintf('Class `%s` using `%s` must implement `%s`', get_class($this), __CLASS__, ScoreAwareInterface::class));
+		}
+		$this->score = $score;
+		return $this;
+	}
+
+}
