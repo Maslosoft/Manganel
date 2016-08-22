eadd84c2df12230290542a79c0a618d8b6561c9a
Peter Maselkowski <pmaselkowski@gmail.com>
2016-08-22 13:17:38 +0200

    * Class headers

--- src/Sanitizers/DateWriteEsSanitizer.php
+++ src/Sanitizers/DateWriteEsSanitizer.php
@@ -1,14 +1,13 @@
 <?php
 
 /**
- * This software package is licensed under AGPL or Commercial license.
+ * This software package is licensed under `AGPL, Commercial` license[s].
  *
- * @package maslosoft/mangan
- * @licence AGPL or Commercial
- * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
- * @copyright Copyright (c) Maslosoft
- * @copyright Copyright (c) Others as mentioned in code
- * @link http://maslosoft.com/mangan/
+ * @package maslosoft/manganel
+ * @license AGPL, Commercial
+ *
+ * @copyright Copyright (c) Peter Maselkowski <pmaselkowski@gmail.com>
+ * @link http://maslosoft.com/manganel/
  */
 
 namespace Maslosoft\Manganel\Sanitizers;
