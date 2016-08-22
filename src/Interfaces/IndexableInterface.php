12997ea82baf23bb317b63588d7be38aac38aeed
Peter Maselkowski <pmaselkowski@gmail.com>
2016-08-22 13:16:27 +0200

    + indexable interface

---
+++ src/Interfaces/IndexableInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Maslosoft\Manganel\Interfaces;
+
+/**
+ * Implement this interface to make document available for
+ * search engine
+ * @SearchIndex
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+interface IndexableInterface
+{
+	
+}
