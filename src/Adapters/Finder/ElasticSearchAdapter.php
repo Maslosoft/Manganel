282377aef5026157ec6a086d3c37ea7c72c4d240
Peter Maselkowski <pmaselkowski@gmail.com>
2016-12-20 19:09:50 +0100

    * Refactor

---
+++ src/Adapters/Finder/ElasticSearchAdapter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Maslosoft\Manganel\Adapters\Fidner;
+
+use Maslosoft\Mangan\Interfaces\Adapters\FinderAdapterInterface;
+use Maslosoft\Mangan\Interfaces\CriteriaInterface;
+
+/**
+ * ElasticSearchAdapter
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class ElasticSearchAdapter implements FinderAdapterInterface
+{
+
+	public function count(CriteriaInterface $criteria)
+	{
+
+	}
+
+	public function findMany(CriteriaInterface $criteria, $fields = array())
+	{
+
+	}
+
+	public function findOne(CriteriaInterface $criteria, $fields = array())
+	{
+
+	}
+
+}
--- src/SearchFinder.php
+++ src/SearchFinder.php
@@ -2,12 +2,48 @@
 
 namespace Maslosoft\Manganel;
 
+use Maslosoft\Addendum\Interfaces\AnnotatedInterface;
+use Maslosoft\Mangan\Abstracts\AbstractFinder;
+use Maslosoft\Mangan\Interfaces\FinderInterface;
+
 /**
  * SearchFinder
  *
  * @author Piotr Maselkowski <pmaselkowski at gmail.com>
  */
-class SearchFinder
+class SearchFinder extends AbstractFinder implements FinderInterface
 {
-	
+
+	/**
+	 * Constructor
+	 *
+	 * @param object $model Model instance
+	 * @param IndexManager $im
+	 * @param Manganel $manganel
+	 */
+	public function __construct($model, $im = null, $manganel = null)
+	{
+		$this->model = $model;
+		$this->sm = new ScopeManager($model);
+		if (null === $manganel)
+		{
+			$manganel = Mangan::fromModel($model);
+		}
+		$this->adapter = new MongoAdapter($model, $manganel, $im);
+		$this->mn = $manganel;
+	}
+
+	/**
+	 * Create model related finder.
+	 *
+	 * @param AnnotatedInterface $model
+	 * @param IndexManager $im
+	 * @param Manganel $manganel
+	 * @return FinderInterface
+	 */
+	public static function create(AnnotatedInterface $model, $im = null, Manganel $manganel = null)
+	{
+		return new static($model, $im, $manganel);
+	}
+
 }
