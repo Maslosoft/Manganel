84f084679bf1d3ffaa580814ea4fba8d9ced270d
Peter Maselkowski <pmaselkowski@gmail.com>
2017-01-23 19:25:42 +0100

    + Max score per document support

---
+++ src/Interfaces/MaxScoreAwareInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This software package is licensed under `AGPL, Commercial` license[s].
+ *
+ * @package maslosoft/manganel
+ * @license AGPL, Commercial
+ *
+ * @copyright Copyright (c) Peter Maselkowski <pmaselkowski@gmail.com>
+ * @link http://maslosoft.com/manganel/
+ */
+
+namespace Maslosoft\Manganel\Interfaces;
+
+/**
+ * Implement this interface to provide max score for:
+ *
+ * * Models via SearchProvider
+ *
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+interface MaxScoreAwareInterface
+{
+
+	public function getMaxScore();
+
+	public function setMaxScore($score);
+}
