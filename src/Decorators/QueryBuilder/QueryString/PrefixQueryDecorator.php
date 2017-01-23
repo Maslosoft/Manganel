f563b02394393b6acce4b80a1e958a35c3f491d9
Peter Maselkowski <pmaselkowski@gmail.com>
2017-01-23 12:00:50 +0100

    * Add wildcard only if not already added

--- src/Decorators/QueryBuilder/QueryString/PrefixQueryDecorator.php
+++ src/Decorators/QueryBuilder/QueryString/PrefixQueryDecorator.php
@@ -7,7 +7,7 @@ use Maslosoft\Manganel\SearchCriteria;
 
 /**
  * This is default decorator for queries, this provides better "As You Type"
- * experience. If it's not deosred, use `QueryDecorator` instead.
+ * experience. If it's not desired, use `QueryDecorator` instead.
  *
  * @see QueryDecorator
  * @author Piotr Maselkowski <pmaselkowski at gmail.com>
@@ -17,7 +17,15 @@ class PrefixQueryDecorator implements QueryStringDecoratorInterface
 
 	public function decorate(&$queryStringParams, SearchCriteria $criteria)
 	{
-		$queryStringParams['query'] = $criteria->getSearch() . '*';
+		$q = $criteria->getSearch();
+
+		// Add `*` only if it does not alredy contain wildcard on end
+		// or will find nothing
+		if (!preg_match('~\*$~', $q))
+		{
+			$q = $q . '*';
+		}
+		$queryStringParams['query'] = $q;
 	}
 
 }
