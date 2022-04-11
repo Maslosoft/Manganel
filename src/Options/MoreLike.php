51b4509203d7c64bfecd9baff041844c6d0307bd
Peter Maselkowski <office@maslosoft.com>
2022-04-11 20:55:38 +0200

    >> Modernize

--- src/Options/MoreLike.php
+++ src/Options/MoreLike.php
@@ -15,11 +15,9 @@ namespace Maslosoft\Manganel\Options;
 use function is_array;
 use Maslosoft\Addendum\Interfaces\AnnotatedInterface;
 use Maslosoft\Mangan\Interfaces\DataProviderInterface;
-use Maslosoft\Manganel\Decorators\QueryBuilder\ConditionDecorator;
 use Maslosoft\Manganel\Helpers\Strings;
 use Maslosoft\Manganel\Helpers\TypeNamer;
 use Maslosoft\Manganel\Manganel;
-use redirect_interval;
 use ReflectionClass;
 
 /**
@@ -32,14 +30,14 @@ class MoreLike
 	 * Models to search from similar
 	 * @var AnnotatedInterface[]
 	 */
-	public $models = [];
+	public array $models = [];
 
 	/**
 	 * A list of fields to fetch and analyze the text from. Defaults to the _all field for free text and to all
 	 * possible fields for document inputs.
 	 * @var array
 	 */
-	public $fields = [];
+	public array $fields = [];
 
 	/**
 	 *
@@ -48,51 +46,51 @@ class MoreLike
 	 * is the same as like.
 	 * @var array
 	 */
-	public $unlike = [];
+	public array $unlike = [];
 
 	/**
 	 * Extra text to search for
 	 * @var string[]
 	 */
-	public $texts = [];
+	public array $texts = [];
 
 	/**
 	 * The maximum number of query terms that will be selected. Increasing this value gives greater accuracy at the
 	 * expense of query execution speed.
 	 * @var int
 	 */
-	public $maxQueryTerms = 25;
+	public int $maxQueryTerms = 25;
 
 	/**
 	 * The minimum term frequency below which the terms will be ignored from the input document.
 	 * @var int
 	 */
-	public $minTermFreq = 2;
+	public int $minTermFreq = 2;
 
 	/**
 	 * The minimum document frequency below which the terms will be ignored from the input document.
 	 * @var int
 	 */
-	public $minDocFreq = 5;
+	public int $minDocFreq = 5;
 
 	/**
 	 * The maximum document frequency above which the terms will be ignored from the input document. This could be
 	 * useful in order to ignore highly frequent words such as stop words
-	 * @var int
+	 * @var int|null
 	 */
-	public $maxDocFreq = null;
+	public ?int $maxDocFreq = null;
 
 	/**
 	 * The minimum word length below which the terms will be ignored. The old name min_word_len is deprecated.
 	 * @var int
 	 */
-	public $minWordLength = 0;
+	public int $minWordLength = 0;
 
 	/**
 	 * The maximum word length above which the terms will be ignored. The old name max_word_len is deprecated.
-	 * @var int
+	 * @var int|null
 	 */
-	public $maxWordLength = null;
+	public ?int $maxWordLength = null;
 
 	/**
 	 * An array of stop words. Any word in this set is considered "uninteresting" and ignored. If the analyzer allows
@@ -100,7 +98,7 @@ class MoreLike
 	 * it seems reasonable to assume that "a stop word is never interesting".
 	 * @var array
 	 */
-	public $stopWords = [];
+	public array $stopWords = [];
 
 	/**
 	 * MoreLikeOptions constructor.
